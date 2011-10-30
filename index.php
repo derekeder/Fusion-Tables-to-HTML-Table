2206c5cb447705e418e89cbb451b247d5524c461
Derek Eder <derek.eder@gmail.com>
2011-10-30 18:25:21 -0500

    using jQuery datatables for sorting, outputting custom columns

--- index.php
+++ index.php
@@ -17,31 +17,77 @@ $result = $ftclient->query(SQLBuilder::select(ConnectionInfo::$fusionTableId));
 //echo $result;
 $csvArr = fthelpers::str_getcsv($result);
 
-//$result = explode("\n", $result);
-
-//foreach ($result as $i => $value) {
-//	//echo "exploding row $i<br />";
-//    $result[$i] = explode(",", $value);
-//}
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Health Clinics in Chicago - Full List</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
 	<link href='/styles/master.css' media='all' rel='stylesheet' type='text/css' />
+	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script> 
 	<script src="/source/analytics_lib.js" type="text/javascript"></script>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<script src="source/jquery.dataTables.min.js" type="text/javascript"></script>
+	
+	<script type="text/javascript">
+	    $(function(){
+	      $(".data").dataTable({
+	        "aaSorting": [[0, "asc"]],
+	        "aoColumns": [
+	          null,
+	          null,
+	          null,
+	          null,
+	          null,
+	          null,
+	          { "bSearchable": false, "bSortable": false }
+	        ],
+	        "bFilter": true,
+	        "bInfo": false,
+	        "bPaginate": false
+	      });
+	    });
+  </script>
 </head>
 <body>
 	<div id="page">
-		<h1>Health Clinics in Chicago - Full List</h1>
+		<h1>Health Clinics in Chicago - List</h1>
+		
+		<p class='tagline'>List of Neighborhood, Mental Health and Women, Infants & Children (WIC) clinics in Chicago. <a href='/maps/chicago-clinics/'>Find them on the map &raquo;</a></p>
+		
+		<table class='data'>
+			<thead>
+				<tr>
+					<th>Clinic</th>
+					<th>Address</th>
+					<th>Type</th>
+					<th>Services</th>
+					<th>Hours</th>
+					<th>Phone</th>
+					<th>Website</th>
+				</tr>
+			</thead>
+			<tbody>
 <?php
 
-echo "<table class='data'>\n";
-
+foreach ($csvArr as $i => $row) {
+	if ($i > 0)
+	{
+		echo "
+		<tr>
+		<td>$row[0]</td>
+		<td>$row[1]</td>
+		<td>$row[10]</td>
+		<td>$row[9]</td>
+		<td>$row[2]</td>
+		<td>$row[7]</td>
+		<td><a href='$row[8]'>Website</a></td>
+		</tr>";
+	}
+}
+/*
 //print table head
 echo "<thead><tr>\n";
 foreach ($csvArr[0] as $i => $col) {
@@ -63,8 +109,12 @@ foreach ($csvArr as $i => $row) {
 }
 
 echo "</tbody></table>\n";
+*/
 
 ?>
+
+			</tbody>
+		</table>
 
 		</div> 
 	</body>
